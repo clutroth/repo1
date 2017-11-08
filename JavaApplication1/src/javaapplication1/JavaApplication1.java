88481cb48bf0afe2090bf81be10e0ceb15d70734
wdk <clutroth@users.noreply.github.com>
2017-11-08 11:15:04 +0100

    komentarze do wersji 1
    Ogólnie spoko. Nie widzę żadnych poważnych błędów.

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -4,25 +4,27 @@
  * and open the template in the editor.
  */
 package javaapplication1;
-import java.util.*;
+import java.util.*; //Importując z gwiazdką, importujesz wszystko, co jest w pakiecie, więc nie wiesz dokładnie co importujesz.
 /**
  *
  * @author Gal Anonim
  */
 public class JavaApplication1 {
-
+//TODO: Spróbuj skompilować i odpalić ten program z konsoli  (polecenia javac i java)
+    //TODO: Wczytaj listę słow do wyświetlenia z konsoli np. java javaaplication1.JavaAplication1 jeden dwa trzy cztey
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        ArrayList<String> slowa = new ArrayList<String>();
+        // deklarując zmienną używaj interfejsów, nie implementacji ArrayList implementuje interfejs List. Poczytaj o listach w Javie. ArrayList, LinkedList
+        ArrayList<String> slowa = new ArrayList<String>();//raczej używaj angielskich słów
         slowa.add("jeden");
         slowa.add("dwa");
         slowa.add("trzy");
         slowa.add("cztery");
+        // Jest metodoa robiąca to w jednej linijce: https://docs.oracle.com/javase/8/docs/api/java/util/Arrays.html#asList-T...-
          Random generator = new Random();
-          int i;
+          int i; // w Javie deklarujemy zmienne jak najbliżej miejsca wykożystania. Ta zmienna powinna być zadeklarowana w pętli.
           for(int j=0; j<100;j++)
           {
           i=generator.nextInt(4);
@@ -30,7 +32,7 @@ public class JavaApplication1 {
           }
     
  
-        
+        //kod nie jest sformatowany. brakuje wcięć. IDE powinno ci to umożłiwiać.
                 
         
         
